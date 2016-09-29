0a127ae2dce4d6ce015de6fab1a917f38d17a2e7
luisbraga91 <matias-f5@hotmail.com>
2016-09-28 21:17:57 -0300

    Add files via upload

---
+++ testa_ssh.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo script criado por LUIS FILIPE MATIAS BRAGA
+
+
+#3) Criar um script chamado testa_ssh.sh que conecta no computador do colega e verifica:
+
+#Qual é o sistema operacional;
+uname -a #este comando irá mostrar qual a arquitetura e o Sistema Operacional que esta acessando.
+
+#Qual o número de bits (32 ou 64) da máquina do colega;
+uname -a #este comando irá mostrar qual a arquitetura e o Sistema Operacional que esta acessando.
